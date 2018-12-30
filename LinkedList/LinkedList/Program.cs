0238c5b81087b35b625d0b7f783d398a56b5b63f
Giancarlo Rhodes <giancarlo.rhodes@gmail.com>
2018-12-30 16:55:56 -0600

    start presentation

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -1,22 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LinkedList
 {
 
-
-
-
     class Program
     {
-   
-
         static void Main(string[] args)
         {
+
+            // +----------+----------+
+            // |    3     |    null  + 
+            // +----------|----------+
+            Node first = new Node { Value = 3 };
+            PrintNode(first);
+
+            // +----------+----------+      +----------+----------+
+            // |    3     |    null  +      |    6     |    null  + 
+            // +----------|----------+      +----------|----------+
+            Node second = new Node { Value = 6 };
+
+            // +----------+----------+      +----------+----------+
+            // |    3     |    *-----+----->|    6     |    null  + 
+            // +----------|----------+      +----------|----------+
+            first.Next = second;
+            PrintNode(second);
+
+
+            // +----------+----------+      +----------+----------+     +----------+----------+
+            // |    3     |    *-----+----->|    6     |    null  +     |    9     |    null  +  
+            // +----------|----------+      +----------|----------+     +----------|----------+
+            Node third = new Node { Value = 9 };
+
+            // +----------+----------+      +----------+----------+     +----------+----------+
+            // |    3     |    *-----+----->|    6     |     *----+---->|    9     |    null  +  
+            // +----------|----------+      +----------|----------+     +----------|----------+
+            second.Next = third;
+            PrintNode(third);
+
+
+
+            Console.ReadKey();
         }
+
+
+
+
+        static void PrintNode(Node inNode)
+        {
+
+            Console.WriteLine("Node value is: " + inNode.Value);
+            
+            Console.WriteLine();
+        }
+
+
+
     }
 
 
